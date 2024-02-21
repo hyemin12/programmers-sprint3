959f2e1b6b98b632e3b7a76a7959ab0c1a143974
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-21 13:33:52 +0900

    style: css reset 추가 작성, font 적용

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
-	return <footer>copyright(c) 2024 hyemin</footer>;
+	return (
+		<FooterStyle>
+			<p>copyright(c) 2024 hyemin</p>
+		</FooterStyle>
+	);
 };
+
+const FooterStyle = styled.footer`
+	width: 100%;
+	padding: 1em 0;
+	background-color: #333;
+	p {
+		font-size: 0.8em;
+		color: #ccc;
+		text-align: center;
+	}
+`;
 
 export default Footer;
--- src/style/global.ts
+++ src/style/global.ts
@@ -1,16 +1,79 @@
 import { createGlobalStyle } from 'styled-components';
 import { ThemeName } from './theme';
+import '@noonnu/pretendard-regular';
 
 interface Props {
 	themeName: ThemeName;
 }
 
 export const GlobalStyle = createGlobalStyle<Props>`
+  *{
+    color:${({ themeName }) => (themeName === 'light' ? '#333' : '#fff')};
+    font-family: "Pretendard-Regular", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif ;
+  }
+  /* 폰트 크기의 팽창을 방지합니다. */
+  html {
+    -moz-text-size-adjust: none;
+    -webkit-text-size-adjust: none;
+    text-size-adjust: none;
+  }
+  /* 고정된 모든 항목에는 여분의 스크롤 여백이 있어야 합니다. */
+  :target {
+    scroll-margin-block: 5ex;
+  }
   body{
     padding:0;
     margin:0;
+    min-height: 100vh;
+    line-height: 1.5;
   }
-  *{
-    color:${({ themeName }) => (themeName === 'light' ? '#333' : '#fff')}
+  /* pico font family 초기화 */
+  h1,
+  h2,
+  h3,
+  h4,h5,h6,
+  p{
+    font-family:"Pretendard-Regular", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif ;
+  }
+  /* 기본 여백을 제거하여 작성된 CSS를 더 잘 제어할 수 있습니다. */
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd {
+    margin-block-end: 0;
+  }
+  /* 제목 요소와 상호작용하는 요소에 대해 line-height를 더 짧게 설정합니다. */
+  h1,
+  h2,
+  h3,
+  h4,
+  button,
+  input,
+  label {
+    line-height: 1.1;
+  }
+  /* 이미지 관련 작업을 더 쉽게 합니다. */
+  img,
+  picture {
+    max-width: 100%;
+    display: block;
+  }
+  /* input 및 button 항목들이 글꼴을 상속하도록 합니다. */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+  /* list를 role값으로 갖는 ul, ol 요소의 기본 목록 스타일을 제거합니다. */
+  ul[role="list"],
+  ol[role="list"] {
+    list-style: none;
   }
 `;
