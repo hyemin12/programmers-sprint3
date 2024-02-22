228c4dc037f4910bb648b61745b1f40e648ef180
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-22 10:59:36 +0900

    feat: 타이틀 컴포넌트 작성

---
+++ src/components/common/Title.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import { ColorKey, HeadingSize } from '../../style/theme';
+
+interface TitleProps {
+  children: React.ReactNode;
+  size: HeadingSize;
+  color?: ColorKey;
+}
+
+const Title = ({ children, size, color }: TitleProps) => {
+  return (
+    <TitleStyle size={size} color={color}>
+      {children}
+    </TitleStyle>
+  );
+};
+const TitleStyle = styled.h1<Omit<TitleProps, 'children'>>`
+  font-size: ${({ theme, size }) => theme.heading[size].fontSize};
+  color: ${({ theme, color }) => (color ? theme.color[color] : theme.color.primary)};
+`;
+export default Title;
