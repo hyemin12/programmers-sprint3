55455dd26588f02a3c121d362b81009edfbd3cab
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-23 11:33:10 +0900

    feat: 회원가입 페이지 작성 및 등록

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Button from 'components/common/Button';
+import InputText from 'components/common/InputText';
+import Title from 'components/common/Title';
+import { Link } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface SignUpProps {
+  email: string;
+  password: string;
+}
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpProps>();
+
+  const onSubmit: SubmitHandler<SignUpProps> = (data: SignUpProps) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Title size="large">회원가입</Title>
+      <SignUpStyle>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset>
+            <InputText
+              placeholder="이메일을 입력하세요"
+              inputType="email"
+              {...register('email', { required: true })}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && <p className="error-text">이메일은 필수로 입력해야합니다.</p>}
+          </fieldset>
+          <fieldset>
+            <InputText
+              placeholder="비밀번호를 입력하세요"
+              inputType="password"
+              {...register('password', { required: true })}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.email && <p className="error-text">비밀번호는 필수로 입력해야합니다.</p>}
+          </fieldset>
+          <Button buttonType="submit" size="medium" scheme="primary">
+            회원가입
+          </Button>
+          <div className="info">
+            <Link to="/reset">비밀번호 초기화</Link>
+          </div>
+        </form>
+      </SignUpStyle>
+    </>
+  );
+};
+
+const SignUpStyle = styled.div`
+  max-width: ${({ theme }) => theme.layout.width.small};
+  margin: 80px auto;
+  fieldset {
+    padding: 0 0 8px 0;
+    border: 0;
+    .error-text {
+      color: tomato;
+    }
+  }
+
+  input {
+    width: 100%;
+  }
+  button {
+    width: 100%;
+  }
+
+  .info {
+    text-align: center;
+    padding: 16px 0 0 0;
+  }
+`;
+
+export default SignUp;
