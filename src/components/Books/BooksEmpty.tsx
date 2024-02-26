e7297a8a2226a70bbcca4ea6eee8217d7f69ffca
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-26 11:05:41 +0900

    feat: 검색결과가 없을 때 보여줄 컴포넌트 생성

--- src/components/Books/BooksEmpty.tsx
+++ src/components/Books/BooksEmpty.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FaSmileWink } from 'react-icons/fa';
+import Title from 'components/common/Title';
+import { Link } from 'react-router-dom';
 
 const BooksEmpty = () => {
-  return <div>BooksEmpty</div>;
+  return (
+    <BooksEmptyStyle>
+      <div className="icon">
+        <FaSmileWink />
+      </div>
+      <Title size="large" color="secondary">
+        검색 결과가 없습니다
+      </Title>
+      <Link to="/books">전체 검색 결과로 이동</Link>
+    </BooksEmptyStyle>
+  );
 };
+
+const BooksEmptyStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  padding: 120px 0;
+  .icon {
+    font-size: 4rem;
+    svg {
+      fill: #ccc;
+    }
+  }
+`;
 
 export default BooksEmpty;
