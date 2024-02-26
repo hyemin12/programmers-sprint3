1fe8739791ec802aa6d0c2f5a66c9f2cb92a5491
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-26 11:43:04 +0900

    feat: 도서 필터링 버튼, 신간 조회 버튼 생성

--- src/hooks/useCategory.ts
+++ src/hooks/useCategory.ts
@@ -1,17 +1,41 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { fetchCategory } from 'api/category.api';
 import { CategoryItem } from 'models/category.model';
+import { QUERYSTRING } from 'constance/querystring';
 
 export const useCategory = () => {
+  const { search } = useLocation();
   const [category, setCategory] = useState<CategoryItem[]>([]);
 
+  const setActive = () => {
+    const params = new URLSearchParams(search);
+    const categoryIdParams = params.get(QUERYSTRING.CATEGORY_ID);
+    if (categoryIdParams) {
+      setCategory((prev) => {
+        return prev.map((item) => ({ ...item, isActive: item.id === Number(categoryIdParams) }));
+      });
+    } else {
+      setCategory((prev) => {
+        return prev.map((item) => ({ ...item, isActive: item.id === null }));
+      });
+    }
+  };
+
+  // 최초 렌더시 실행
   useEffect(() => {
     fetchCategory().then((response) => {
       if (!response) return;
       const categoryWithAll = [{ id: null, name: '전체' }, ...response];
       setCategory(categoryWithAll);
+      setActive();
     });
   }, []);
+
+  // url이 변경될 때 setActive 함수 실행
+  useEffect(() => {
+    setActive();
+  }, [search]);
 
   return category;
 };
