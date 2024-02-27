1e7fe0d42195d5e0af97b46515bc15097fddad4c
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-27 13:33:21 +0900

    feat: 날짜 포맷하는 유틸함수 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,1 +1,7 @@
+import dayjs from 'dayjs';
+
 export const formatNumber = (number: number) => number.toLocaleString();
+
+export const formatDate = (date: string, format?: string) => {
+  return dayjs(date).format(format ? format : 'YYYY년 MM월 DD일');
+};
