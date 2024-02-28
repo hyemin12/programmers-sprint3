81c473da5c131ddc245ab3c63ff12bc34c33442e
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-28 16:50:49 +0900

    feat: 주문내역 가져오기, 상세 주문내역 가져오기 기능 추가

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -4,11 +4,24 @@ export interface IOrder {
   id: number;
   created_at: string;
   address: string;
-  receiver: string;
+  recipient: string;
   contact: string;
   book_title: string;
   total_quantity: number;
   total_price: number;
+  payment: string;
+}
+
+export interface IOrderDetail {
+  book_id: number;
+  book_title: string;
+  author: string;
+  price: number;
+  quantity: number;
+}
+
+export interface IOrderListItem extends IOrder {
+  detail?: IOrderDetail[];
 }
 
 export interface IOrderedBook {
