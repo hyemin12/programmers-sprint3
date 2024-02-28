ad784a9d6779c972c9cabb804b1be157e5d11b48
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-28 12:05:00 +0900

    fix: 잘못된 경로로 이동하는 오류 수정

--- src/components/AddToCartButton/index.tsx
+++ src/components/AddToCartButton/index.tsx
@@ -58,7 +58,7 @@ const AddToCartButton = ({ book, quantity }: AddToCartButtonProps) => {
 
       <div className="added">
         <p>장바구니에 추가되었습니다</p>
-        <Link to="/carts">장바구니로 이동</Link>
+        <Link to="/cart">장바구니로 이동</Link>
       </div>
     </AddToCartButtonStyle>
   );
