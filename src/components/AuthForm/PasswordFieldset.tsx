24821c4d73a84897d93ec8e26f6b240b70a9bfc5
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-03-11 15:21:40 +0900

    feat: 비밀번호 폼 유효성검사 로직 추가

--- src/components/AuthForm/PasswordFieldset.tsx
+++ src/components/AuthForm/PasswordFieldset.tsx
@@ -7,9 +7,20 @@ const PasswordFieldset = ({ register, errors }: FieldsetProps) => {
       <InputText
         placeholder="비밀번호를 입력하세요"
         inputType="password"
-        {...register('password', { required: true })}
+        {...register('password', {
+          required: true,
+          minLength: { value: 8, message: '비밀번호는 최소 8글자여야 합니다.' },
+          maxLength: {
+            value: 16,
+            message: '비밀번호는 최대 16글자입니다.',
+          },
+        })}
       />
-      {errors.password && <p className="error-text">비밀번호는 필수로 입력해야합니다.</p>}
+      {errors.password && (
+        <p className="error-text">
+          {errors.password.message ? errors.password.message : '비밀번호는 필수로 입력해야합니다.'}
+        </p>
+      )}
     </fieldset>
   );
 };
