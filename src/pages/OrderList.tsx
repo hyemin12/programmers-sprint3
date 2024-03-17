1d914a9a3a3b0e2a010643f60a0c3475a2d49dae
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-03-17 12:39:53 +0900

    refactor: 주문내역이 없을 때 처리, 컴포넌트 import 정리

--- src/pages/OrderList.tsx
+++ src/pages/OrderList.tsx
@@ -1,36 +1,40 @@
 import styled from 'styled-components';
+import { FaLaughWink } from 'react-icons/fa';
 import { useOrders } from 'hooks/useOrders';
-import Title from 'components/common/Title';
-import OrderItem from '../components/OrderList/OrderItem';
+import { OrderItem } from 'components/OrderList';
+import { Empty, Title } from 'components/common';
 
 const OrderList = () => {
   const { orders } = useOrders();
   return (
     <>
       <Title size="large">주문 내역</Title>
-      <OrderListStyle>
-        <table>
-          <thead>
-            <tr>
-              <th>no</th>
-              <th>주문일자</th>
-              <th>주소</th>
-              <th>수령인</th>
-              <th>전화번호</th>
-              <th>대표상품명</th>
-              <th>수량</th>
-              <th>금액</th>
-              <th>지불방법</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order, idx) => (
-              <OrderItem order={order} index={idx + 1} key={order.id} />
-            ))}
-          </tbody>
-        </table>
-      </OrderListStyle>
+      {orders.length === 0 && <Empty icon={<FaLaughWink />} title="주문내역이 없습니다." />}
+      {orders.length > 0 && (
+        <OrderListStyle>
+          <table>
+            <thead>
+              <tr>
+                <th>no</th>
+                <th>주문일자</th>
+                <th>주소</th>
+                <th>수령인</th>
+                <th>전화번호</th>
+                <th>대표상품명</th>
+                <th>수량</th>
+                <th>금액</th>
+                <th>지불방법</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order, idx) => (
+                <OrderItem order={order} index={idx + 1} key={order.id} />
+              ))}
+            </tbody>
+          </table>
+        </OrderListStyle>
+      )}
     </>
   );
 };
